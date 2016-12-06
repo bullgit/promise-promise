5e8ae528033b6f3c879edd9530222130df584372
René Viering <rene.viering@gmail.com>
2016-12-07 00:27:25 +0100

    Implement randomy fulfilling promise functionality

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+import excuses from './excuses';
+
+const getRandomExcuse = () => {
+    return excuses[Math.floor(Math.random() * excuses.length)];
+};
+
+
 export default class PromisePromise {
-    
+	constructor(init) {
+		const promise = new Promise(init);
+		return new Promise((resolve, reject) => {
+			promise
+				.then(response => {
+					const shouldResolve = Math.random() >= 0.5;
+					if (shouldResolve) {
+						resolve(response);
+					} else {
+						reject(getRandomExcuse());
+					}
+                    resolve(response);
+				})
+                .catch(err => {
+                    resolve(err);
+                });
+		});
+	}
 }
